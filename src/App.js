d5dfd8577cfc62ae648f9e12eee401a8afbb6287
Erencan KARADAĞ <64350262+ekaradag14@users.noreply.github.com>
2021-12-20 18:54:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
+import { useEffect } from 'react';
+
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		const elem = document.querySelector('#find');
+
+		elem.scrollIntoView({
+			behavior: 'smooth',
+			block: 'center',
+			inline: 'center',
+		});
+	}, []);
+	return (
+		<div className="App">
+			<header className="App-header"></header>
+			<div style={{ height: 1200 }}></div>
+			<div
+				id="find"
+				style={{ height: 400, width: 400, backgroundColor: 'green' }}
+			></div>
+		</div>
+	);
 }
 
 export default App;
